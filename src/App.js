433279555879999784c12d99ae9ef99bdf83f604
Bruno Henrique <brunohenriquescosta@hotmail.com>
2020-04-12 12:52:31 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response =>{
+      setRepositories(response.data);
+    });
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+   const response = await api.post('repositories',{
+    title: "new repo",
+    url:"https://github.com/Bruno-hsc/Gympoint_Front_end",
+    techs: ["react", "node", "Native"],
+    likes:0
+   });
+
+   const repository = response.data;
+
+   setRepositories([ ...repositories, repository ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+   await api.delete(`/repositories/${id}`);
+    
+   setRepositories(repositories.filter(
+    r => r.id !== id
+  ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(
+          r => <li key={r.id}>{r.title}  
+          <button onClick={() => handleRemoveRepository(r.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
